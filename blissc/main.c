79c4a8f334b0a661adb0f8a41fe1fa849a9c5dc3
Matthew Madison <madison@iMac-Matt.local>
2012-10-22 09:15:11 -0700

    Initial Commit

---
+++ blissc/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  blissc
+//
+//  Created by Matthew Madison on 10/22/12.
+//  Copyright (c) 2012 Matthew Madison. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
