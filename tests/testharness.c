4a498ee315babe505a3424df4b13c7a1760c9edb
Matt Madison <madison@athrat.org>
2013-02-24 10:01:19 -0800

    Provide for specifying the test numbers in the test harness.

--- tests/testharness.c
+++ tests/testharness.c
@@ -1,4 +1,15 @@
+/*++
+ * testharness.c
+ *
+ * Simple test harness for running BLISS tests.
+ *
+ * Copyright © 2013, Matthew Madison.
+ * All rights reserved.
+ * Distributed under license. See LICENSE.TXT for details.
+ *--
+ */
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
 
 int TEST_INIT(void);
@@ -16,12 +27,23 @@ TEST_PRINTF (const char *fmt, ...)
 }
 
 int
-main (void)
+main (int argc, char *argv[])
 {
     int i, numtests;
 
     numtests = TEST_INIT();
-    for (i = 1; i <= numtests; i++)
-        RUNTEST(i);
+    if (argc < 2) {
+        for (i = 1; i <= numtests; i++)
+            RUNTEST(i);
+        return 0;
+    }
+
+    for (i = 1; i < argc; i++) {
+        int tno = atoi(argv[i]);
+        if (tno >= 1 && tno <= numtests) {
+            RUNTEST(tno);
+        }
+    }
+
     return 0;
 }
