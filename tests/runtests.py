88175d41da5ca598bed2ff277bba4bbebc7b7941
Matt Madison <matt@madison.systems>
2020-07-22 09:22:05 -0700

    runtests: update for python 3
    Signed-off-by: Matt Madison <matt@madison.systems>

--- tests/runtests.py
+++ tests/runtests.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # runtests
 #
 #   Runs BLISS compiler tests.
@@ -6,9 +6,9 @@
 #   Tests are BLISS source files with comments embedded that
 #   indicate the expected results.
 #
-#   N.B.: requires Python 2.7 or later.
+#   N.B.: requires Python 3.
 #
-# Copyright (c) 2013, Matthew Madison
+# Copyright (c) 2013-2020, Matthew Madison
 # All rights reserved.
 # Distributed under license; see LICENSE.TXT for details.
 
@@ -24,6 +24,7 @@ tried = 0
 passed = 0
 failed = 0
 
+
 def process_test(testfile, blissc, cc, testharness, quiet=False, prefix='!!'):
     if not quiet:
         print("Processing: {}".format(testfile))
@@ -32,37 +33,43 @@ def process_test(testfile, blissc, cc, testharness, quiet=False, prefix='!!'):
     runout = []
     failures = 0
     cases = 0
-    f = open(testfile, 'r')
-    for lraw in f.readlines():
-        l = lraw.strip()
-        if l.startswith(prefix):
-            s = l[len(prefix):].strip()
-            (cmd, junk, result) = s.partition(' ')
-            if cmd == 'cout':
-                compout.append(result.strip())
-            elif cmd == 'cerr':
-                comperrs.append(result.strip())
-            else:
-                try:
-                    caseno = int(cmd)
-                except ValueError:
-                    caseno = 0
-                if caseno != 0:
-                    cases = cases + 1
-                    runout.append('TEST {:03d}: {}'.format(caseno,result.strip()))
-    f.close()
+    with open(testfile, 'r') as infile:
+        caselist = [line.rstrip() for line in infile.readlines() if line.startswith(prefix)]
+    for case in caselist:
+        s = case[len(prefix):].strip()
+        cmd, junk, result = s.partition(' ')
+        if cmd == 'cout':
+            compout.append(result.strip())
+        elif cmd == 'cerr':
+            comperrs.append(result.strip())
+        else:
+            try:
+                caseno = int(cmd)
+            except ValueError:
+                caseno = 0
+            if caseno != 0:
+                cases = cases + 1
+                runout.append('TEST {:03d}: {}'.format(caseno, result.strip()))
 
     barename = os.path.splitext(os.path.basename(testfile))[0]
-    docompile = subprocess.Popen([blissc, '-o', barename + '.o',
-                                  '-I', '.', testfile],
-                                 stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                                 universal_newlines=True)
-    (oraw, eraw) = docompile.communicate()
-    o = oraw.split('\n')
-    e = []
-    for l in eraw.split('\n'):
-        if l.startswith('%'):
-            e.append(l)
+    try:
+        docompile = subprocess.run([blissc, '-o', barename + '.o', '-I', '.', testfile],
+                                   capture_output=True, text=True, check=True)
+        o = docompile.stdout.split('\n')
+        if docompile.stderr:
+            e = [line for line in docompile.stderr.split('\n') if line.startswith('%')]
+        else:
+            e = []
+    except subprocess.CalledProcessError as err:
+        if err.returncode > 0 and len(comperrs) > 0:
+            o = err.stdout.split('\n')
+            e = [line for line in err.stderr.split('\n') if line.startswith('%')]
+            pass
+        else:
+            print(("  FAIL: compilation failed ({}) for {}" +
+                   "\n        {}").format(err.returncode, testfile,
+                                          '\n        '.join([line for line in err.stderr.split('\n')])))
+            return cases, 0, cases
 
     for s in compout:
         if s in o:
@@ -75,8 +82,8 @@ def process_test(testfile, blissc, cc, testharness, quiet=False, prefix='!!'):
         failures = failures + 1
         if not quiet:
             print("  FAIL: unexpected compilation errors:")
-            for l in e:
-                print("         {}".format(l))
+            for errline in e:
+                print("         {}".format(errline))
     else:
         for s in comperrs:
             if len(e) == 0:
@@ -94,11 +101,20 @@ def process_test(testfile, blissc, cc, testharness, quiet=False, prefix='!!'):
     if not quiet and failures == 0:
         print("  compilation phase passed")
 
-    subprocess.call([cc, '-o', barename, testharness, barename + '.o'])
-    dorun = subprocess.Popen(['./' + barename], stdout=subprocess.PIPE,
-                             universal_newlines=True)
-    oraw = dorun.communicate()[0]
-    o = oraw.split('\n')
+    try:
+        subprocess.run([cc, '-o', barename, testharness, barename + '.o'], check=True)
+    except subprocess.CalledProcessError as e:
+        print(("  FAIL: linking {} into test harness failed" +
+              "\n        {}").format(barename + '.o', '\n        '.join([line for line in e.stderr.split('\n')])))
+        return cases, 0, cases
+    try:
+        dorun = subprocess.run(['./' + barename], capture_output=True, text=True, check=True)
+    except subprocess.CalledProcessError as e:
+        print(("  FAIL: test harness failed to run for {}" +
+              "\n        {}").format(testfile, '\n        '.join([line for line in e.stderr.split('\n')])))
+        return cases, 0, cases
+
+    o = dorun.stdout.split('\n')
     passes = 0
     for s in runout:
         if s in o:
@@ -108,9 +124,9 @@ def process_test(testfile, blissc, cc, testharness, quiet=False, prefix='!!'):
             failures = failures + 1
             pfx = s.partition(':')
             actual = ""
-            for l in o:
-                if l.startswith(pfx):
-                    actual = l
+            for outline in o:
+                if outline.startswith(pfx):
+                    actual = outline
                     break
             if actual == "":
                 actual = "<not found>"
@@ -121,23 +137,23 @@ def process_test(testfile, blissc, cc, testharness, quiet=False, prefix='!!'):
                 print("         actual: {}".format(actual))
 
     if not quiet:
-        print("  {:d} of {:d} test cases passed".format(passes,cases))
-    return (cases, passes, failures)
+        print("  {:d} of {:d} test cases passed".format(passes, cases))
+    return cases, passes, failures
 
 
 # begin main
 
-(mydir, myname) = os.path.split(sys.argv[0])
+mydir, myname = os.path.split(sys.argv[0])
 parser = argparse.ArgumentParser(description='Run blissc unit tests', prog=myname)
 parser.add_argument('testorsuite', help='test file name or test suite directory', nargs='*', default='.')
 parser.add_argument('--blissc', help='location of BLISS compiler', default='blissc')
 parser.add_argument('--harness', help='test harness C file', default=os.path.join(mydir, 'testharness.c'))
-parser.add_argument('--cc', help="C compiler", default='cc')
+parser.add_argument('--cc', help="C compiler", default=os.getenv("CC") or "cc")
 parser.add_argument('--quiet', '-q', action='store_true')
 
 args = parser.parse_args()
 
-pat = re.compile('^.+\.bli$')
+pat = re.compile(r'^.+\.bli$')
 for t_or_s in args.testorsuite:
     if os.path.exists(t_or_s):
         if os.path.isdir(t_or_s):
@@ -156,7 +172,7 @@ totcases = 0
 totpasses = 0
 for t in tests:
     tried = tried + 1
-    (c, p, f) = process_test(t, args.blissc, args.cc, args.harness, args.quiet)
+    c, p, f = process_test(t, args.blissc, args.cc, args.harness, args.quiet)
     totcases = totcases + c
     totpasses = totpasses + p
     if f == 0:
